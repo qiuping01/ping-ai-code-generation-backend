c5930335d924b24e911415f26c06d5c3495bdde6
qiuping01 <2187718660@qq.com>
2025-11-17 15:11:30 +0800

    用户模块 - 配置 MyBatis Flex

--- src/main/java/com/ping/pingaicodegeneration/PingAiCodeGenerationApplication.java
+++ src/main/java/com/ping/pingaicodegeneration/PingAiCodeGenerationApplication.java
@@ -1,9 +1,11 @@
 package com.ping.pingaicodegeneration;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ping.pingaicodegeneration.mapper")
 public class PingAiCodeGenerationApplication {
 
     public static void main(String[] args) {
